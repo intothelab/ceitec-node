ab3920e60eaf90c1b253bb48743817c97ba88b00
Samuel Cecilio <samuel@cecilio.org>
2019-08-19 15:04:40 -0300

    Improve errorHandler and use custom file path

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
-import axiosRetry from "axios-retry";
-import path from "path";
-import fs from "fs";
+// tslint:disable: no-console prefer-const array-type
 
-axiosRetry(axios, { retries: 3 });
-axios.defaults.withCredentials = true;
+import axios, { AxiosError, AxiosResponse } from "axios";
+import fs from "fs";
+// import path from "path";
 
 interface IFile {
   id: string;
   timestamp: string;
   antenna: string;
+}
+
+const errorHandler = (error: any) => {
+  let message = error.message;
+  let timestamp = new Date().toISOString();
+
+  console.log(`${timestamp}: ${message}`);
 };
 
 const asyncForEach = async (arr: any, cb: any) => {
   for (let index = 0; index < arr.length; index++) {
     await cb(arr[index], index, arr);
   }
-}
-
-const errorHandler = (error: any) => {
-  console.log(error);
-}
+};
 
 const parseLine = (line: Array<string>) => {
   return {
-    "id": line[0],
-    "timestamp": line[1],
-    "antenna": line[2],
+    id: line[0],
+    timestamp: line[1],
+    antenna: line[2],
   };
 };
 
@@ -49,7 +50,7 @@ const parseFile = async (file: any) => {
 
 const sendToServer = (data: Array<IFile>) => {
   axios.post("http://localhost:3000", data)
-    .then((response: AxiosResponse) => console.log('OK'))
+    .then((response: AxiosResponse) => console.log("OK"))
     .catch((error: AxiosError) => errorHandler(error));
 
   // console.log(data);
@@ -64,6 +65,7 @@ const processFile = async (error: any, file: any) => {
 };
 
 setInterval(() => {
-  const file = path.join(__dirname, "../example.txt");
+  const file = "C:\\RAUL\\log_demo_ceitec.txt"; // path.join(__dirname, "../example.txt");
+
   fs.readFile(file, "utf8", processFile);
 }, 15000);
